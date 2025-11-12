c7e15b2a2fae9416b50415cfdde2db365d620a48
IsoGraff98 <111709792+IsoGraff98@users.noreply.github.com>
2025-11-12 08:00:39 -0500

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,11 +9,16 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0 ;
+    cart.forEach(item => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1)); // "$10.00" → 10.00
+        total += quantity * cost;
+    });
   };
 
   const handleContinueShopping = (e) => {
-   
+
   };
 
 
